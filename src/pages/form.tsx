0371ee3df4ca85970792884199345071972c9f41
torikatsupg <torikatsu923@gmail.com>
2023-05-27 14:22:55 +0900

    Implement form with react-hook-form

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,7 +1,23 @@
+import { useForm } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+}
+
 export default function Form() {
+  const {register, watch, handleSubmit, formState: {errors}} = useForm<Inputs>();
+  watch(console.log);
+
+  const onSubmit = console.log
+
   return (
     <>
-      
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>input text</label>
+        <input type="text" {...register("name")} />
+
+        <input type="submit" />
+      </form>
     </>
   );
 }
