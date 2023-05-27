bd7eef35c570edf698b4512ed94d1a717df9cac9
torikatsupg <torikatsu923@gmail.com>
2023-05-27 15:26:37 +0900

    use zodResolver

--- src/pages/form.tsx
+++ src/pages/form.tsx
@@ -1,22 +1,46 @@
 import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 
-type Inputs = {
-  name: string;
-}
+const schema = z
+  .object({
+    email: z.string().email(),
+  })
+  .required({
+    email: true,
+  });
+
+type Form = z.infer<typeof schema>;
 
 export default function Form() {
-  const {register, watch, handleSubmit, formState: {errors}} = useForm<Inputs>();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    setError,
+    reset,
+  } = useForm<Form>({
+    resolver: zodResolver(schema),
+  });
+
   watch(console.log);
 
-  const onSubmit = console.log
+  const onsubmit = (data: any) => {
+    console.log(data);
+    setError("email", { type: "torikatsu", message: "custom error" });
+
+    reset();
+  };
 
   return (
     <>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onsubmit)} noValidate>
         <label>input text</label>
-        <input type="text" {...register("name")} />
+        <input type="email" {...register("email")} />
+        <p>{errors.email?.message}</p>
 
-        <input type="submit" />
+        <button>submit</button>
       </form>
     </>
   );
